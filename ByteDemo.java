5d9701129b68d10296fcf08acc60aa08596c93ee
Tia-beanie <144304264+Tia-beanie@users.noreply.github.com>
2024-11-14 11:59:03 -0800

    Add files via upload

---
+++ ByteDemo.java
@@ -0,0 +1,10 @@
+public class ByteDemo {
+    public static void main(String[] args){
+        byte number;
+        int data = 128;
+
+        number = (byte)data;
+
+        System.out.println(" number = " + number);
+    }
+}
